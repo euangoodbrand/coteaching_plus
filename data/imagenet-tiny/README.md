43dd93fe082deb29695ab51af9be5fdbe0d49fdf
Euan Goodbrand <euangoodbrand@aol.com>
2024-04-04 01:12:40 +0100

    Working Co-Teaching Plus for NIDS data on CIC-IDS-2017 dataset.
    Working Co-Teaching Plus for NIDS data on CIC-IDS-2017 dataset.
    
    Initial code, quite rough.

--- data/imagenet-tiny/README.md
+++
@@ -1,3 +0,0 @@
-Download imagenet-tiny dataset from : https://tiny-imagenet.herokuapp.com/.
-
-Then unzip it to here. 
--- main.py
+++ main.py
@@ -6,18 +6,35 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import torchvision.transforms as transforms
-from data.cifar import CIFAR10, CIFAR100
-from data.mnist import MNIST
-from data.newsgroups import NewsGroups
-from data.torchlist import ImageFilelist
-from model import MLPNet, CNN_small, CNN, NewsNet
-from preact_resnet import PreActResNet18
+from model import MLPNet
 import argparse, sys
 import numpy as np
 import datetime
-import shutil
-
+from tqdm import tqdm
 from loss import loss_coteaching, loss_coteaching_plus
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt # plotting
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from torch.utils.data import Dataset, DataLoader
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import torch.nn.init as init
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from imblearn.over_sampling import SMOTE
+
+for dirname, _, filenames in os.walk('/data'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+nRowsRead = None 
+
+
+import os
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--lr', type = float, default = 0.001)
@@ -26,17 +43,17 @@ parser.add_argument('--noise_rate', type = float, help = 'corruption rate, shoul
 parser.add_argument('--forget_rate', type = float, help = 'forget rate', default = None)
 parser.add_argument('--noise_type', type = str, help='[pairflip, symmetric]', default='symmetric')
 parser.add_argument('--num_gradual', type = int, default = 10, help='how many epochs for linear drop rate. This parameter is equal to Ek for lambda(E) in the paper.')
-parser.add_argument('--dataset', type = str, help = 'mnist, cifar10, cifar100, or imagenet_tiny', default = 'mnist')
+parser.add_argument('--dataset', type = str, help = 'cicids', default = 'cicids')
 parser.add_argument('--n_epoch', type=int, default=200)
 parser.add_argument('--optimizer', type = str, default='adam')
 parser.add_argument('--seed', type=int, default=1)
-parser.add_argument('--print_freq', type=int, default=50)
-parser.add_argument('--num_workers', type=int, default=4, help='how many subprocesses to use for data loading')
+parser.add_argument('--print_freq', type=int, default=1)
+parser.add_argument('--num_workers', type=int, default=1, help='how many subprocesses to use for data loading')
 parser.add_argument('--epoch_decay_start', type=int, default=80)
 parser.add_argument('--model_type', type = str, help='[coteaching, coteaching_plus]', default='coteaching_plus')
 parser.add_argument('--fr_type', type = str, help='forget rate type', default='type_1')
-
 args = parser.parse_args()
+
 
 # Seed
 torch.manual_seed(args.seed)
@@ -45,122 +62,57 @@ torch.cuda.manual_seed(args.seed)
 # Hyper Parameters
 batch_size = 128
 learning_rate = args.lr 
-
-# load dataset
-if args.dataset=='mnist':
-    input_channel = 1
-    init_epoch = 0
-    num_classes = 10
-    args.n_epoch = 200
-    train_dataset = MNIST(root='./data/',
-                                download=True,  
-                                train=True, 
-                                transform=transforms.ToTensor(),
-                                noise_type=args.noise_type,
-                                noise_rate=args.noise_rate
-                                )
-    
-    test_dataset = MNIST(root='./data/',
-                               download=True,  
-                               train=False, 
-                               transform=transforms.ToTensor(),
-                               noise_type=args.noise_type,
-                               noise_rate=args.noise_rate
-                                )
-    
-if args.dataset=='cifar10':
-    input_channel=3
-    init_epoch = 20
-    num_classes = 10
-    args.n_epoch = 200
-    train_dataset = CIFAR10(root='./data/',
-                                download=True,  
-                                train=True, 
-                                transform=transforms.ToTensor(),
-                                noise_type=args.noise_type,
-                                noise_rate=args.noise_rate
-                                )
-    
-    test_dataset = CIFAR10(root='./data/',
-                                download=True,  
-                                train=False, 
-                                transform=transforms.ToTensor(),
-                                noise_type=args.noise_type,
-                                noise_rate=args.noise_rate
-                                )
-
-if args.dataset=='cifar100':
-    input_channel=3
-    init_epoch = 5
-    num_classes = 100
-    args.n_epoch = 200
-    train_dataset = CIFAR100(root='./data/',
-                                download=True,  
-                                train=True, 
-                                transform=transforms.ToTensor(),
-                                noise_type=args.noise_type,
-                                noise_rate=args.noise_rate
-                                )
-    
-    test_dataset = CIFAR100(root='./data/',
-                                download=True,  
-                                train=False, 
-                                transform=transforms.ToTensor(),
-                                noise_type=args.noise_type,
-                                noise_rate=args.noise_rate
-                                )
+init_epoch = 0
 
 
-if args.dataset=='news':
-    init_epoch=0
-    train_dataset = NewsGroups(root='./data/',
-                                train=True, 
-                                transform=transforms.ToTensor(),
-                                noise_type=args.noise_type,
-                                noise_rate=args.noise_rate
-                                )
-    
-    test_dataset = NewsGroups(root='./data/',
-                               train=False, 
-                               transform=transforms.ToTensor(),
-                               noise_type=args.noise_type,
-                               noise_rate=args.noise_rate
-                                )
-    num_classes=train_dataset.num_classes
- 
-if args.dataset == 'imagenet_tiny':
-    init_epoch = 100
-    #data_root = '/home/xingyu/Data/phd/data/imagenet-tiny/tiny-imagenet-200'
-    data_root = 'data/imagenet-tiny/tiny-imagenet-200'
-    train_kv = "train_noisy_%s_%s_kv_list.txt" % (args.noise_type, args.noise_rate) 
-    test_kv = "val_kv_list.txt"
 
-    normalize = transforms.Normalize(mean=[0.4802, 0.4481, 0.3975], 
-                                     std =[0.2302, 0.2265, 0.2262])
+def introduce_label_noise(labels, noise_rate=args.noise_rate):
+    n_samples = len(labels)
+    n_noisy = int(noise_rate * n_samples)
+    noisy_indices = np.random.choice(np.arange(n_samples), size=n_noisy, replace=False)
 
-    train_dataset = ImageFilelist(root=data_root, flist=os.path.join(data_root, train_kv),
-               transform=transforms.Compose([transforms.RandomResizedCrop(56),
-               transforms.RandomHorizontalFlip(),
-               transforms.ToTensor(), 
-               normalize,
-       ]))
+    # Initialize as all False, indicating no sample is noisy initially
+    noise_or_not = np.zeros(n_samples, dtype=bool)  
 
-    test_dataset = ImageFilelist(root=data_root, flist=os.path.join(data_root, test_kv),
-               transform=transforms.Compose([transforms.Resize(64),
-               transforms.CenterCrop(56),
-               transforms.ToTensor(),
-               normalize,
-       ]))
+    # Iterate over the randomly selected indices to introduce noise
+    unique_labels = np.unique(labels)
+    for idx in noisy_indices:
+        original_label = labels[idx]
+        # Exclude the original label to ensure the new label is indeed different
+        possible_labels = np.delete(unique_labels, np.where(unique_labels == original_label))
+        # Randomly select a new label from the remaining possible labels
+        new_label = np.random.choice(possible_labels)
+        labels[idx] = new_label  # Assign the new label
+        noise_or_not[idx] = True  # Mark this index as noisy
+
+    return labels, noise_or_not
+
+class CICIDSDataset(Dataset):
+    def __init__(self, features, labels, noise_or_not):
+        self.features = features
+        self.labels = labels
+        self.noise_or_not = noise_or_not 
+
+    def __getitem__(self, index):
+        feature = torch.tensor(self.features[index], dtype=torch.float32)
+        label = torch.tensor(self.labels[index], dtype=torch.long)
+        return feature, label, index  
+
+    def __len__(self):
+        return len(self.labels)
+
+
+
+
+
 
 if args.forget_rate is None:
     forget_rate=args.noise_rate
 else:
     forget_rate=args.forget_rate
 
-if args.dataset == 'imagenet_tiny':
-    noise_or_not = np.load(os.path.join(data_root, 'noise_or_not_%s_%s.npy' %(args.noise_type, args.noise_rate)))
-else:
-    noise_or_not = train_dataset.noise_or_not
+
+
 
 # Adjust learning rate and betas for Adam Optimizer
 mom1 = 0.9
@@ -181,12 +133,6 @@ def gen_forget_rate(fr_type='type_1'):
     if fr_type=='type_1':
         rate_schedule = np.ones(args.n_epoch)*forget_rate
         rate_schedule[:args.num_gradual] = np.linspace(0, forget_rate, args.num_gradual)
-
-    #if fr_type=='type_2':
-    #    rate_schedule = np.ones(args.n_epoch)*forget_rate
-    #    rate_schedule[:args.num_gradual] = np.linspace(0, forget_rate, args.num_gradual) 
-    #    rate_schedule[args.num_gradual:] = np.linspace(forget_rate, 2*forget_rate, args.n_epoch-args.num_gradual)
-        
     return rate_schedule
 
 rate_schedule = gen_forget_rate(args.fr_type)
@@ -202,7 +148,6 @@ txtfile = save_dir + "/" + model_str + ".txt"
 nowTime=datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S')
 if os.path.exists(txtfile):
     os.system('mv %s %s' % (txtfile, txtfile+".bak-%s" % nowTime))
-
 
 def accuracy(logit, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
@@ -220,17 +165,18 @@ def accuracy(logit, target, topk=(1,)):
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
 
-# Train the Model
-def train(train_loader,epoch, model1, optimizer1, model2, optimizer2):
-    print('Training %s...' % model_str)
-    
-    train_total=0
-    train_correct=0 
-    train_total2=0
-    train_correct2=0 
 
-    for i, (data, labels, indexes) in enumerate(train_loader):
-        ind=indexes.cpu().numpy().transpose()
+
+def train(train_dataset, train_loader, epoch, model1, optimizer1, model2, optimizer2, noise_or_not):
+    print('Training %s...' % model_str)
+    train_total = 0
+    train_correct = 0
+    train_total2 = 0
+    train_correct2 = 0
+
+    for i, (data, labels, indices) in enumerate(train_loader):
+
+        ind=indices.cpu().numpy().transpose()
       
         labels = Variable(labels).cuda()
         
@@ -303,7 +249,130 @@ def evaluate(test_loader, model1, model2):
     acc2 = 100*float(correct2)/float(total2)
     return acc1, acc2
 
+
+def weights_init(m):
+    if isinstance(m, nn.Linear):
+        # Apply custom initialization to linear layers
+        init.xavier_uniform_(m.weight.data)  # Xavier initialization for linear layers
+        if m.bias is not None:
+            init.constant_(m.bias.data, 0)    # Initialize bias to zero
+
+    elif isinstance(m, nn.Conv2d):
+        # Apply custom initialization to convolutional layers
+        init.kaiming_normal_(m.weight.data)   # Kaiming initialization for convolutional layers
+        if m.bias is not None:
+            init.constant_(m.bias.data, 0)     # Initialize bias to zero
+
+
+
 def main():
+
+    preprocessed_file_path = 'final_dataframe.csv'
+
+    if os.path.exists(preprocessed_file_path):
+        print("Concatonated dataset already exists")
+        # Load the preprocessed DataFrame from the saved CSV file
+        df = pd.read_csv(preprocessed_file_path)
+    else:
+        print("Dataset doesn't exists: generating...")
+
+        df1 = pd.read_csv("data/cicids2017/MachineLearningCSV/MachineLearningCVE/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv")
+        df2=pd.read_csv("data/cicids2017/MachineLearningCSV/MachineLearningCVE/Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv")
+        df3=pd.read_csv("data/cicids2017/MachineLearningCSV/MachineLearningCVE/Friday-WorkingHours-Morning.pcap_ISCX.csv")
+        df4=pd.read_csv("data/cicids2017/MachineLearningCSV/MachineLearningCVE/Monday-WorkingHours.pcap_ISCX.csv")
+        df5=pd.read_csv("data/cicids2017/MachineLearningCSV/MachineLearningCVE/Thursday-WorkingHours-Afternoon-Infilteration.pcap_ISCX.csv")
+        df6=pd.read_csv("data/cicids2017/MachineLearningCSV/MachineLearningCVE/Thursday-WorkingHours-Morning-WebAttacks.pcap_ISCX.csv")
+        df7=pd.read_csv("data/cicids2017/MachineLearningCSV/MachineLearningCVE/Tuesday-WorkingHours.pcap_ISCX.csv")
+        df8=pd.read_csv("data/cicids2017/MachineLearningCSV/MachineLearningCVE/Wednesday-workingHours.pcap_ISCX.csv")
+        df = pd.concat([df1, df2, df3, df4, df5, df6, df7, df8], ignore_index=True)
+
+        # nRowsRead = 20000  # specify 'None' to read all rows
+        # df6 = pd.read_csv("data/cicids2017/MachineLearningCSV/MachineLearningCVE/Thursday-WorkingHours-Morning-WebAttacks.pcap_ISCX.csv", nrows=nRowsRead)
+        # df = pd.concat([df6], ignore_index=True)
+
+        df.reset_index(drop=True, inplace=True)
+        nRow, nCol = df.shape
+        df.columns = df.columns.str.strip()
+        print("Saving concatonated data")
+
+        df.to_csv(preprocessed_file_path, index=False)
+
+    if os.path.exists('numpy_data/features.npy'):
+        print("Preprocessed data exists, loading...")
+        features_np_standardized = np.load('numpy_data/features.npy')
+        labels_np = np.load('numpy_data/labels.npy')
+        indices = np.load('numpy_data/indices.npy')
+        X_train_smote = np.load('numpy_data/X_train.npy')
+        y_train_smote = np.load('numpy_data/y_train.npy')
+        X_test = np.load('numpy_data/X_test.npy')
+        y_test = np.load('numpy_data/y_test.npy')
+        test_indices = np.load('numpy_data/test_indices.npy')  
+    else:
+        print("Preprocessed data doesn't exists, generating...")
+
+        # Convert your DataFrame columns to numpy arrays if not already done
+        features_np = df.drop('Label', axis=1).values.astype(np.float32)  
+        labels_np = LabelEncoder().fit_transform(df['Label'].values)
+
+        # Check for inf and -inf values
+        print("Contains inf: ", np.isinf(features_np).any())
+        print("Contains -inf: ", np.isneginf(features_np).any())
+
+        # Check for NaN values
+        print("Contains NaN: ", np.isnan(features_np).any())
+
+        # Replace inf/-inf with NaN
+        features_np[np.isinf(features_np) | np.isneginf(features_np)] = np.nan
+
+        # Impute NaN values with the median of each column
+        imputer = SimpleImputer(strategy='median')
+        features_np_imputed = imputer.fit_transform(features_np)
+
+        # Initialize the StandardScaler
+        scaler = StandardScaler()
+
+        # Fit on the imputed features data and transform it to standardize
+        features_np_standardized = scaler.fit_transform(features_np_imputed)
+
+        # Generate indices for your dataset, which will be used for splitting
+        indices = np.arange(len(labels_np))
+
+        # Split indices into training and testing sets
+        train_indices, test_indices, y_train, y_test = train_test_split(indices, labels_np, test_size=0.3, random_state=42)
+
+        # Correctly split the standardized and imputed dataset
+        X_train, X_test, y_train, y_test = train_test_split(features_np_standardized, labels_np, test_size=0.3, random_state=42)
+
+        # Apply SMOTE on the correctly preprocessed training data
+        smote = SMOTE(random_state=42)
+        X_train_smote, y_train_smote = smote.fit_resample(X_train, y_train)
+
+
+        # Introduce label and feature noise AFTER splitting
+        labels_noisy, noise_or_not = introduce_label_noise(labels_np, noise_rate=0.2)  # Adjust as per your function's implementation
+
+
+        # Save the numpy arrays
+        print("Saving preprocessd data")
+
+        np.save('numpy_data/features.npy', features_np_standardized)
+        np.save('numpy_data/labels.npy', labels_np)
+        np.save('numpy_data/indices.npy', indices)
+        np.save('numpy_data/X_train.npy', X_train_smote)
+        np.save('numpy_data/y_train.npy', y_train_smote)
+        np.save('numpy_data/X_test.npy', X_test)
+        np.save('numpy_data/y_test.npy', y_test)
+        np.save('numpy_data/test_indices.npy', test_indices)
+
+
+    noise_or_not = np.zeros(len(y_train_smote), dtype=bool)  
+
+    # Create the PyTorch datasets with the SMOTE-applied variables
+    train_dataset = CICIDSDataset(X_train_smote, y_train_smote, noise_or_not)
+    test_dataset = CICIDSDataset(X_test, y_test, noise_or_not[test_indices]) # No SMOTE on test data
+
+    noise_or_not = train_dataset.noise_or_not
+
     # Data Loader (Input Pipeline)
     print('loading dataset...')
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
@@ -319,35 +388,31 @@ def main():
                                               shuffle=False)
     # Define models
     print('building model...')
-    if args.dataset == 'mnist':
-        clf1 = MLPNet()
-    if args.dataset == 'cifar10':
-        clf1 = CNN_small(num_classes)
-    if args.dataset == 'cifar100':
-        clf1 = CNN(n_outputs=num_classes)
-    if args.dataset=='news':
-        clf1 = NewsNet(weights_matrix=train_dataset.weights_matrix, num_classes=num_classes)
-    if args.dataset=='imagenet_tiny':
-        clf1 = PreActResNet18(num_classes=200)
 
+    torch.manual_seed(234565678)
+    clf1 = MLPNet()
+    clf1.apply(weights_init)
     clf1.cuda()
-    print(clf1.parameters)
     optimizer1 = torch.optim.Adam(clf1.parameters(), lr=learning_rate)
-    
-    if args.dataset == 'mnist':
-        clf2 = MLPNet()
-    if args.dataset == 'cifar10':
-        clf2 = CNN_small(num_classes)
-    if args.dataset == 'cifar100':
-        clf2 = CNN(n_outputs=num_classes)
-    if args.dataset=='news':
-        clf2 = NewsNet(weights_matrix=train_dataset.weights_matrix, num_classes=num_classes)
-    if args.dataset=='imagenet_tiny':
-        clf2 = PreActResNet18(num_classes=200)
 
+
+    torch.manual_seed(45456678)  # Change the seed to get a different initialization
+    clf2 = MLPNet()
+    clf2.apply(weights_init)
+
+    # Move models to GPU
     clf2.cuda()
-    print(clf2.parameters)
+
+    # Define optimizers
     optimizer2 = torch.optim.Adam(clf2.parameters(), lr=learning_rate)
+    print(clf1.parameters)
+
+    print(clf2.parameters)
+
+
+    result_dir_path = os.path.dirname(txtfile)
+    if not os.path.exists(result_dir_path):
+        os.makedirs(result_dir_path)
 
     with open(txtfile, "a") as myfile:
         myfile.write('epoch train_acc1 train_acc2 test_acc1 test_acc2\n')
@@ -364,14 +429,14 @@ def main():
 
     # training
     for epoch in range(1, args.n_epoch):
-        # train models
+        # train 
         clf1.train()
         clf2.train()
 
         adjust_learning_rate(optimizer1, epoch)
         adjust_learning_rate(optimizer2, epoch)
 
-        train_acc1, train_acc2 = train(train_loader, epoch, clf1, optimizer1, clf2, optimizer2)
+        train_acc1, train_acc2 = train(train_dataset, train_loader, epoch, clf1, optimizer1, clf2, optimizer2, noise_or_not)
         # evaluate models
         test_acc1, test_acc2 = evaluate(test_loader, clf1, clf2)
         # save results
